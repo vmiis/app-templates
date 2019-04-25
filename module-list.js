162835d4f268d418507dde0bbae695d53b95322b
Zhiming Zhou <zzm.vmiis@gmail.com>
2019-04-26 06:49:49 +1000

    update

--- module-list.js
+++ module-list.js
@@ -1,21 +1,22 @@
 (function(){
     //-------------------------------------------------------------------------------------
-    var url_replace=function(modules,prefix,$A,$R,$L){
+    var url_replace=function(modules,prefix,$H,$R,$L){
         for(m in modules){
             var $S=$R; if($vm.localhost==true) $S=$L;
-            modules[m].url=modules[m].url.replace($A,$S); 
+            modules[m].url=modules[m].url.replace($H,$S); 
             $vm.module_list[prefix+m]=modules[m]; 
             $vm.module_list[prefix+m].prefix=prefix; 
-        } 
+        } ``
     }
     //-------------------------------------------------------------------------------------
     var modules={
-        "a-simple-application":  		{url:"$A/a/a-simple-application/index.html"},
-        "aaxsys":  		                {url:"$A/a/aaxsys/index.html"},
-        "bookkeeping":  		        {url:"$A/b/bookkeeping/index.html"},
-        "bookkeeping-wappsystem":       {url:"$A/b/bookkeeping-wappsystem/index.html"},
-        "bookkeeping-vmautomation":     {url:"$A/b/bookkeeping-vmautomation/index.html"},
+        "a-simple-application":  		{url:"$H/a/a-simple-application/index.html"},
+        "aaxsys":  		                {url:"$H/a/aaxsys/index.html"},
+        "bookkeeping":  		        {url:"$H/b/bookkeeping/index.html"},
+        "bookkeeping-for-wappsystem":   {url:"$H/b/bookkeeping-wappsystem/index.html"},
+        "bookkeeping-for-vmautomation": {url:"$H/b/bookkeeping-vmautomation/index.html"},
+        "particle-background":          {url:"$H/p/particle-background/index.html"},
     }
-    url_replace(modules,"","$A","https://app-templates.vmiis.com/","http://127.0.0.1:8000/vmiis/app-templates");
+    url_replace(modules,"","$H","https://app-templates.vmiis.com/","http://127.0.0.1:8000/vmiis/app-templates");
     //-------------------------------------------------------------------------------------
 })();
